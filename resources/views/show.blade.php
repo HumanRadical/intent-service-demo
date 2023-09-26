8a0f628a1350eda1970b5e136b8c71e71df2cd7d
Easton Meth <easton.meth@gmail.com>
2023-09-26 17:08:00 -0400

    Extract table component

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -23,67 +23,6 @@
             <h3 class="font-semibold text-2xl">Children:</h3>
             <a class="my-auto text-lg text-gray-600 hover:underline" href="/">View Full</a>
         </div>
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-3">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            ID
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Date
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Donor ID
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Donor Name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Donor Email
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Intent
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Parent
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Outcome
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{ $intent->id }}
-                        </th>
-                        <td class="px-6 py-4">
-                            <time>{{ $intent->created_at->format('M d, h:iA T') }}</time>
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $intent->user->id }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $intent->user->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $intent->user->email }}
-                        </td>
-                        <td class="px-6 py-4">
-                                <a href="/intent/{{ $intent->id }}">
-                                    {{ $intent->name }}
-                                </a>
-                            </td>
-                        <td class="px-6 py-4">
-                            <a class="hover:underline">Parent</a>
-                        </td>
-                        <td class="px-6 py-4 text-{{ $intent->successful ? "green" : "red" }}-500">
-                            {{ $intent->successful ? "✓ Success" : "✕ Declined" }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <x-table :intents="[$intent]" />
     </div>
 </x-layout>
