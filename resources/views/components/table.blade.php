e0a60012b7bcfa7cab55a35a3bf6746d18d380df
Easton Meth <easton.meth@gmail.com>
2023-09-26 17:18:15 -0400

    Make table striped

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -32,7 +32,7 @@
         </thead>
         <tbody>
             @foreach ($intents as $intent)
-                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <tr class="bg-{{ $loop->iteration % 2 === 0 ? "gray-50" : "white" }} border-b dark:bg-gray-900 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{ $intent->id }}
                     </th>
