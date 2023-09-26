3fdaeb1f15fcf4b9a43ea93359b69469783d88db
Easton Meth <easton.meth@gmail.com>
2023-09-26 14:55:33 -0400

    Create index markup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,64 @@
 <x-layout>
-    Hi!
+    <h1 class="text-center text-4xl font-semibold">Intent Service</h1>
+    <div class="border rounded-xl w-2/3 p-6 mx-auto mt-4">
+        <form class="flex justify-center gap-4" action="/">
+            <input class="border border-gray-500 rounded-md w-80 px-2 py-1" type="text" placeholder="Search...">
+            <input class="border border-gray-500 rounded-md px-2 py-1" type="date">
+            <button class="border border-gray-500 bg-gray-200 rounded-md px-3 py-1">Search</button>
+        </form>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Date
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Donor Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Donor Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Intent
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Parent
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Outcome
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            1
+                        </th>
+                        <td class="px-6 py-4">
+                            <time>Oct 10, 2023 @ 8:00pm</time>
+                        </td>
+                        <td class="px-6 py-4">
+                            Johnny Test
+                        </td>
+                        <td class="px-6 py-4">
+                            jtest@example.com
+                        </td>
+                        <td class="px-6 py-4">
+                            Make a Donation
+                        </td>
+                        <td class="px-6 py-4">
+                            <a class="hover:underline">Use Giving App</a>
+                        </td>
+                        <td class="px-6 py-4 text-green-500">
+                            Success
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </x-layout>
