4a0d679aad41dd3887f9d993023adce10173c9b8
Easton Meth <easton.meth@gmail.com>
2023-09-28 14:34:59 -0400

    Remove parent show page

--- resources/views/parent/show.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <div class="flex text-3xl mb-6">
-        <button class="text-2xl my-auto font-semibold border-2 border-black rounded-full px-1" onclick="history.back()">&#8592;</button>
-        <h2 class="mx-auto my-auto font-bold">{{ $parent->name }}</h2>
-    </div>
-    <x-table :intents="$parent->intents" />
-</x-layout>
